1b22841a985a06a2cdc1b8ac27ab2bb9b45cd728
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-08-23 18:57:32 +0530

    add todo function created

--- todo.js
+++ todo.js
@@ -1,21 +1,26 @@
 let todos = [
     {
         list_name : "Default",
+        id: 0,
         todo_item : ["Buy Milk", "Go for Walk", "Be active"]
     },
     {
         list_name : "Shopping",
+        id:1,
         todo_item : ["Buy a study chair", "Buy a keyboard", "Buy a house"]
     },
     {
         list_name :"Youtube",
+        id:2,
         todo_item : ["Complete Dynamic programming", "Bit Manupulation"]
     }
 ]
 
-
+let currentSelectedListIndex = 0;
 
 const todo_container = document.getElementById("todo_items");
+
+
 function renderTodo(){
     todo_container.innerHTML = "";
     todos.map(todo => {
@@ -47,13 +52,15 @@ function renderList(){
     allList.classList.add("active_list");
     let html = `
         <p>All Lists</p>
-        <p class="list_count">${todos.length}</p>
+        
     `
     allList.innerHTML = html;
     list_container.appendChild(allList);
     todos.map(todo => {
         let section = document.createElement("section");
+        section.setAttribute("id", `${todo.id}`)
         section.classList.add("list");
+        section.setAttribute("onclick","renderSpecificListTodos(this.id)")
         let html = `
             <p>${todo.list_name}</p>
             <p class="todo_count">${todo.todo_item.length}</p>
@@ -64,3 +71,36 @@ function renderList(){
     })
 }
 renderList();
+
+
+function renderSpecificListTodos(listId){
+    currentSelectedListIndex = listId;
+    todo_container.innerHTML = "";
+    todos[listId].todo_item.map(item => {
+       let list_name = todos[listId].list_name;
+            let todo_item = document.createElement("div");
+            todo_item.classList.add("todo_item");
+            let html = `
+                <div class="checkbox">
+                    <span>&check;</span>
+                </div>
+                <section>
+                    <p>${item}</p>
+                    <p>${list_name}</p>
+                </section>
+            `
+            todo_item.innerHTML = html;
+            todo_container.appendChild(todo_item)
+    })
+}
+
+//new todo 
+
+let newTodoInput = document.querySelector("#todo_input");
+function addNewTodo(){
+    todos[currentSelectedListIndex].todo_item.push(newTodoInput.value);
+    renderSpecificListTodos(currentSelectedListIndex);
+    renderList();
+}
+
+
